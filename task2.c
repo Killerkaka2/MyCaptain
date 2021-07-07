6c6da5d673f7baf1f184b1c65c922695fdc70332
Killerkaka2 <87017284+Killerkaka2@users.noreply.github.com>
2021-07-07 19:35:07 +0530

    Update task2.c

--- task2.c
+++ task2.c
@@ -3,10 +3,13 @@ int main()
 {
 	int a,b,t;
 	printf("Enter 2 nos\n");
-	scanf("%d%d",&a,&b);
+	printf("a=");
+	scanf("%d\n",&a);
+	printf("b=");
+	scanf("%d\n",&b);
 	t=a;
 	a=b;
 	b=t;
-	printf("Now the values after swapping is: %d\n %d",a,b);
+	printf("Now the values after swapping is: a= %d\n b=%d",a,b);
 	return 0;
 }
